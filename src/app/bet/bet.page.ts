39dd0ff2404e15324c1a775bdf4906de8d9ac70e
mlikoga <marcelolikoga@gmail.com>
2019-02-17 15:17:59 -0300

    Driver class

--- src/app/bet/bet.page.ts
+++ src/app/bet/bet.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Driver } from '../model/driver';
 
 @Component({
   selector: 'app-bet',
@@ -8,24 +9,19 @@ import { Component } from '@angular/core';
 export class BetPage {
 
   positions: Array<Number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  drivers: Array<Object> = [];
+  drivers: Array<Driver> = [];
   
   constructor() {
     this.drivers = [
-      { 'id': 44, 'name': 'Lewis Hamilton' },
-      { 'id': 77, 'name': 'Valtteri Bottas' },
-      { 'id': 5, 'name': 'Sebstian Vettel' },
-      { 'id': 16, 'name': 'Charles Leclerc' },
-      { 'id': 35, 'name': 'Max Verstappen' },
-      { 'id': 3, 'name': 'Daniel Ricciardo' },
-      { 'id': 27, 'name': 'Nico Hulkenberg' },
-      { 'id': 8, 'name': 'Romain Grosjean' },
-      { 'id': 20, 'name': 'Kevin Magnussen' }
+      { id: 44, name: 'Lewis Hamilton' },
+      { id: 77, name: 'Valtteri Bottas' },
+      { id:  5, name: 'Sebstian Vettel' },
+      { id: 16, name: 'Charles Leclerc' },
+      { id: 35, name: 'Max Verstappen' },
+      { id:  3, name: 'Daniel Ricciardo' },
+      { id: 27, name: 'Nico Hulkenberg' },
+      { id:  8, name: 'Romain Grosjean' },
+      { id: 20, name: 'Kevin Magnussen' }
     ];
   }
-
-  driverSelected(value) {
-    console.log(`Driver selected ${value}`)
-  }
-
-}
+ }

05fef68e1e915bdff62d39845e244a0079888f76
mlikoga <marcelolikoga@gmail.com>
2019-03-12 22:22:42 -0300

    delete dummy data

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -10,19 +10,5 @@ export class Tab3Page {
   users: Array<Object> = []
   
   constructor() {
-    this.users = [
-      { 'name': 'Koga', 'points': 300},
-      { 'name': 'Possebon', 'points': 264},
-      { 'name': 'Matsumoto', 'points': 209},
-      { 'name': 'Bruno Maranhão', 'points': 208},
-      { 'name': 'César Castro', 'points': 200},
-      { 'name': 'Yuri', 'points': 197},
-      { 'name': 'Jonathan Almeida', 'points': 190},
-      { 'name': 'Renato Martella', 'points': 183},
-      { 'name': 'Danilo Mayer', 'points': 182},
-      { 'name': 'Luiz Maciel', 'points': 180},
-      { 'name': 'PC', 'points': 176},
-      { 'name': 'Bigas', 'points': 170},
-    ];
   }
 }

892f7fba9eed126eae67e6301f41d1eab6c96320
mlikoga <marcelolikoga@gmail.com>
2019-03-14 01:27:11 -0300

    users collection

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
-
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  db: firebase.firestore.Firestore;
   currentUsername: string;
 
   constructor(private storage: Storage, private platform: Platform) {
@@ -15,12 +17,23 @@ export class AuthService {
         this.currentUsername = value;
       })
     });
+    this.db = firebase.firestore();
   }
 
   login(username: string) : void {
     this.currentUsername = username;
     this.storage.set('login', username);
+    this.db.collection('users').doc(username).set({
+      username: username,
+      last_logged_in: new Date(),
+    });
     console.log("Login successful!");
+  }
+  
+  logout() : void {
+    this.currentUsername = null;
+    this.storage.set('login', null);
+    console.log("Logout");
   }
 
   currentUser() : string {

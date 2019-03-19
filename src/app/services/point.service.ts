b2b18956814ca5df9bf72cdeebbf95e4b3a5d061
mlikoga <marcelolikoga@gmail.com>
2019-03-18 23:56:27 -0300

    point service

---
+++ src/app/services/point.service.ts
@@ -0,0 +1,34 @@
+import { Bet } from '../model/bet';
+
+export class PointService {
+
+  static racePoints = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1];
+
+  static calculatePoints(result: Bet, bet: Bet): number {
+    let points = 0;
+    if(result.pole === bet.pole) {
+      points += 10;
+    }
+    if(result.fastestLap === bet.fastestLap) {
+      points += 10;
+    }
+    for (let i = 0; i < result.positions.length; i++) {
+      let result_i = result.positions[i];
+      if (result_i === bet.positions[i]) {
+        points += this.racePoints[i];
+      } else if (result_i === bet.positions[i-1] || result_i === bet.positions[i+1]) {
+        points += 0.6 * this.racePoints[i];
+      } else if (result_i === bet.positions[i-2] || result_i === bet.positions[i+2]) {
+        points += 0.3 * this.racePoints[i];
+      } else if (result_i === bet.positions[i-3] || result_i === bet.positions[i+3]) {
+        points += 0.1 * this.racePoints[i];
+      }
+
+      if (bet.positions.includes(result_i)) {
+        points += 2;
+      }
+    }
+    return points;
+  }
+
+}

d0fa3522e4aa61f39f046802e893973bfad9e5e1
mlikoga <marcelolikoga@gmail.com>
2019-03-26 23:10:38 -0300

    bettingEnabled

--- src/app/services/time.service.spec.ts
+++ src/app/services/time.service.spec.ts
@@ -2,22 +2,45 @@ import { TimeService } from './time.service';
 import { Race } from '../model/race';
 import * as moment from 'moment';
 
-fdescribe('nextRace', () => {
-
+describe('nextRace', () => {
   let timeService = new TimeService();
 
   it('returns Australia at 2019-01-01', () => {
     let date = moment('2019-01-01T12:00:00Z');
-    expect(timeService.nextRace(date).name).toBe('Australia');
+    expect(timeService.currentRace(date).name).toBe('Australia');
   });
 
   it('still returns Australia at 2019-03-26', () => {
     let date = moment('2019-03-26T12:00:00Z');
-    expect(timeService.nextRace(date).name).toBe('Australia');
+    expect(timeService.currentRace(date).name).toBe('Australia');
   });
 
   it('returns Bahrein at 2019-03-27', () => {
     let date = moment('2019-03-27T12:00:00Z');
-    expect(timeService.nextRace(date).name).toBe('Bahrein');
+    expect(timeService.currentRace(date).name).toBe('Bahrein');
+  });
+});
+
+describe('bettingEnabled', () => {
+  let timeService = new TimeService();
+
+  it('return false at Friday more than 1 week before GP', () => {
+    let date = moment('2019-03-21T12:00:00Z');
+    expect(timeService.bettingEnabled(date)).toBeFalsy();
+  });
+
+  it('return false at Tuesday before GP', () => {
+    let date = moment('2019-03-26T12:00:00Z');
+    expect(timeService.bettingEnabled(date)).toBeFalsy();
+  });
+
+  it('return true at Wednesday before GP', () => {
+    let date = moment('2019-03-27T12:00:00Z');
+    expect(timeService.bettingEnabled(date)).toBeTruthy();
+  });
+
+  it('return false at Saturday before GP', () => {
+    let date = moment('2019-03-30T12:00:00Z');
+    expect(timeService.bettingEnabled(date)).toBeFalsy();
   });
 });

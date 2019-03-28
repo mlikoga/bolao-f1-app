c742dd4ac4ed7a4a6b9a8998f2e96e459422b004
mlikoga <marcelolikoga@gmail.com>
2019-03-27 23:39:07 -0300

    name fix

--- src/app/services/time.service.spec.ts
+++ src/app/services/time.service.spec.ts
@@ -7,12 +7,12 @@ describe('nextRace', () => {
 
   it('returns Australia at 2019-01-01', () => {
     let date = moment('2019-01-01T12:00:00Z');
-    expect(timeService.currentRace(date).name).toBe('Australia');
+    expect(timeService.currentRace(date).name).toBe('Austrália');
   });
 
   it('still returns Australia at 2019-03-26', () => {
     let date = moment('2019-03-26T12:00:00Z');
-    expect(timeService.currentRace(date).name).toBe('Australia');
+    expect(timeService.currentRace(date).name).toBe('Austrália');
   });
 
   it('returns Bahrein at 2019-03-27', () => {

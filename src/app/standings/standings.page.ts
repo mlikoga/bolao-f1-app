848b52995d0b6dfe75c9d4775bda10d7d5bf60e9
mlikoga <marcelolikoga@gmail.com>
2019-04-04 22:24:44 -0300

    userservice

--- src/app/standings/standings.page.ts
+++ src/app/standings/standings.page.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { User } from '../model/user';
 import { ResultService } from '../services/result.service';
-import * as firebase from 'firebase';
-import 'firebase/firestore';
 import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-standings',
@@ -12,12 +11,11 @@ import { AuthService } from '../services/auth.service';
 })
 export class StandingsPage {
 
-  db: firebase.firestore.Firestore;
   users: Array<User> = [];
   currentUser: string;
 
-  constructor(private authService: AuthService, private resultService: ResultService) { 
-    this.db = firebase.firestore();
+  constructor(private authService: AuthService, private resultService: ResultService,
+    private userService: UserService) { 
   }
   
   ionViewWillEnter() {
@@ -28,16 +26,12 @@ export class StandingsPage {
     this.refresh();
   }
 
-  refresh(event?) : void {
-    this.users = [];
-    this.db.collection("users").get().then((querySnapshot) => {
-      querySnapshot.forEach(async (doc) => {
-          let user = doc.data() as User;
-          user.points = await this.resultService.getTotalPoints(user.username);
-          this.users.push(user);
-          this.users.sort((u1, u2) => u2.points - u1.points);
-          if (event) event.target.complete();
-      });
+  async refresh(event?) : Promise<void> {
+    this.users = await this.userService.getUsers();
+    this.users.forEach(async (user) => {
+        user.points = await this.resultService.getTotalPoints(user.username);
     });
+    this.users.sort((u1, u2) => u2.points - u1.points);
+    if (event) event.target.complete();
   }
 }

669c9f6dea4f7675d6504b86bf5479be41f88772
mlikoga <marcelolikoga@gmail.com>
2019-10-02 00:14:53 -0300

    RELEASE v1.18

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -28,7 +28,7 @@ export class ProfilePage implements OnInit {
     public toastController: ToastController,
     private swUpdate: SwUpdate) {
 
-    this.version = "1.17";
+    this.version = "1.18";
     this.user = new User('','');
   }
 

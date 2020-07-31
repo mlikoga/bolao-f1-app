274f944d5fc8e5517320a97652f55ad1c202fea2
mlikoga <marcelo.koga@nubank.com.br>
2020-07-31 10:11:35 -0300

    Hulkenberg

--- src/app/model/driver.ts
+++ src/app/model/driver.ts
@@ -18,14 +18,15 @@ export class Driver {
       { id: "OCO", name: "Esteban Ocon",       number: 31, team: "Renault" },
       { id: "GAS", name: "Pierre Gasly",       number: 10, team: "AlphaTauri" },
       { id: "KVY", name: "Daniil Kvyat",       number: 26, team: "AlphaTauri" },
-      { id: "PER", name: "Sergio Perez",       number: 11, team: "Racing Point" },
       { id: "STR", name: "Lance Stroll",       number: 18, team: "Racing Point" },
+      { id: "HUL", name: "Nico Hulkenberg",    number: 27, team: "Racing Point" },
       { id: "RAI", name: "Kimi Raikkonen",     number:  7, team: "Alfa Romeo Racing" },
       { id: "GIO", name: "Antonio Giovinazzi", number: 99, team: "Alfa Romeo Racing" },
       { id: "GRO", name: "Romain Grosjean",    number:  8, team: "Haas F1 Team" },
       { id: "MAG", name: "Kevin Magnussen",    number: 20, team: "Haas F1 Team" },
       { id: "RUS", name: "George Russell",     number: 63, team: "Williams" },
       { id: "LAT", name: "Nicholas Latifi",    number:  6, team: "Williams" },
+      { id: "PER", name: "Sergio Perez",       number: 11, team: "Racing Point" },
     ]
   }
 

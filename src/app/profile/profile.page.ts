7ab9d87a8c6385ba408126daef3a4e12701ab936
Marcelo Li Koga <marcelo.koga@nubank.com.br>
2024-02-19 13:58:33 -0300

    version update

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -33,7 +33,7 @@ export class ProfilePage implements OnInit {
     private toastController: ToastController,
     private swUpdate: SwUpdate) {
 
-    this.version = "6.0.0-alpha";
+    this.version = "6.0.0-alpha2";
     this.user = new User('','');
   }
 
